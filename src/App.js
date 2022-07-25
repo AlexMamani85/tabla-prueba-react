d171eb38ca008cc8f237f17b5dbd35cb1bdb773e
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-07-25 10:47:45 -0500

    CHORE: fetch data with setUsers to Users

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import DataTable from 'react-data-table-component'
-
+import React from 'react'
 import './App.css';
 
 function App() {
 
+  const [users, setUsers] = React.useState([])
 
+  function fetchData(){
+    const options = {method: 'GET', mode: 'cors', headers: {'Access-Control-Allow-Origin':'*'}};
+    
+    fetch('https://randomuser.me/api/?page=3&results=45&seed=abc', options)
+      .then(response => response.json())
+      .then(response => setUsers(response))
+      .catch(err => console.error(err));
+  }
+
+  fetchData();
+  
+  // setUsers(fetchData());
+  console.log(users);
 const data = [
   {
     id: 1,
