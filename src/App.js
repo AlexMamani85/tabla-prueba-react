9ee85262a149f43e71045bfe87f0bf7ef3d12eb6
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-07-25 10:17:33 -0500

    CHORE: test react-data-table-component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
+import DataTable from 'react-data-table-component'
 
 import './App.css';
 
 function App() {
+
+
+const data = [
+  {
+    id: 1,
+    name: 'Juan Carlos',
+    age: 25
+  },
+    {
+    id: 2,
+    name: 'Martina',
+    age: 29
+  },
+    {
+    id: 3,
+    name: 'Canelaria',
+    age: 36
+  }
+]
+const columns = [
+  {
+  name: 'ID',
+  selector: row => row.age
+  },
+    {
+  name: 'NAME',
+  selector: row => row.age
+  },
+    {
+  name: 'AGE',
+  selector: row => row.age
+  },
+]
+
   return (
     <div className="App">
-
+      <DataTable
+        columns={columns}
+        data={data}
+        
+      ></DataTable>
     </div>
   );
 }
